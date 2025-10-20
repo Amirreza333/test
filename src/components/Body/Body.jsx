a1a7a14c1f9ab28b282803da0c3f6747f1c74ec8
amir <jwwwnamyr6@gmail.com>
2025-10-20 02:22:23 -0700

    second code

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,30 +1,58 @@
-// import Style from "./body.module.css";
-import stlye from "../Body/body.module.css";
+import style from "../Body/body.module.css";
+import { PiCheckCircleFill } from "react-icons/pi";
+import { BiSolidPhoneCall } from "react-icons/bi";
+
 function Body() {
+  const items = [
+    "خدمات جامع",
+    "فناوری پیشرفته",
+    "ارتباطات شفاف",
+  ];
+
   return (
-    <div className={stlye.container}>
-    <div className={stlye.bodypicture}>
-        <img className={stlye.image} src="/public/Picture/OIP.webp" alt="" />
-        <div className={stlye.bodytext}>
-          <h5 className="matn1">درباره ما</h5>
+    <div className={style.container}>
+      <div className={style.content}>
+        
+        <div className={style.textSection}>
+          <h5 className={style.subtitle}>درباره ما</h5>
+          <h1 className={style.title}>ساختن رویاها با دقت و کیفیت</h1>
+
+          <p className={style.description}>
+            ساخت سازه‌هایی که مادام‌العمر دوام می‌آورند، نیازمند رویکردی کل‌نگر است که
+            مواد پیشرفته، طراحی انعطاف‌پذیر، نگهداری منظم و شیوه‌های پایداری را ادغام می‌کند.
+            با یادگیری از نمونه‌های تاریخی و بهره‌گیری از تکنولوژی مدرن.
+          </p>
+
+          <ul className={style.checklist}>
+     
+
+            {items.map((item, index) => (
+              <li key={index} className={style.checkItem}>
+                <PiCheckCircleFill className={style.checkIcon} />
+                <span>{item}</span>
+              </li>
+            ))}
+                   <div className={style.cta}>
+  <button className={style.btn}>مشاهده بیشتر</button>
+  <div className={style.phoneBox}>
+    <span className={style.phoneNumber}> <BiSolidPhoneCall /> 
+ 021-3333333</span>
+  </div>
+</div>
+          </ul>
         </div>
-        <h1 className="matn2">ساختن رویاها با دقت و کیفیت</h1>
-        <div>
-          <p className={stlye.description}></p>
-          <h4 className="matn3">
-            ساخت سازه‌هایی که مادام‌العمر دوام می‌آورند، نیازمند رویکردی کل‌نگر
-            است که مواد پیشرفته، طراحی انعطاف‌پذیر، نگهداری منظم و شیوه‌های
-            پایداری را ادغام می‌کند. با یادگیری از نمونه های تاریخی و بهره گیری
-            از تکنولوژی مدرن
-          </h4>
-          <div className={stlye.box}>
-            <li className="boxtext1">خدمات جامع</li>
-            <li className="boxtext2">فن آوری پیشرفته</li>
-            <li className="boxtext3">ارتباطات شفاف</li>
+
+        
+        <div className={style.imageSection}>
+          <img
+            className={style.image}
+            src="/Picture/OIP.webp"
+            alt="تصویر ساختمان"
+          />
         </div>
       </div>
     </div>
-          </div>
   );
 }
+
 export default Body;
