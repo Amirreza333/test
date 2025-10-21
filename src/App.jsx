1d9866b7b63d9d0fdcea8e478c0839fb54a3e5d8
amir <jwwwnamyr6@gmail.com>
2025-10-21 02:55:25 -0700

    more body

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Body from "./components/Body/Body";
 import Header from "./components/header/Header";
 import Card from "./components/Card/Card"
 import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs"
+import ProjectsSection from "./components/ProjectsSection/ProjectsSection"
 function App() {
   return (
     <div>
@@ -9,6 +10,7 @@ function App() {
       <Body />
      <Card/>
      <WhyChooseUs/>
+     <ProjectsSection/>
     </div>
   );
 }
---
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./ProjectSection.module.css";
+
+const projects = [
+  { id: 1, img: "/public/Picture/beatiful buildings (3).jpg", title: "پارک ویو پلازا", desc: "پروژه‌ای مدرن با طراحی شگفت‌انگیز و چشم‌انداز شهری بی‌نظیر. هر جزئیات آن با دقت برنامه‌ریزی شده است." },
+  { id: 2, img: "/public/Picture/beatiful buildings (1).jpg", title: "اقامتگاه بایساید", desc: "اقامتگاهی لوکس در کنار ساحل با امکانات کامل و چشم‌انداز آرامش‌بخش دریا. ایده‌آل برای زندگی مدرن و آرامش‌بخش." },
+  { id: 3, img: "/public/Picture/beatiful buildings (2).jpg", title: "برج های جنگلی تپه", desc: "برج‌هایی مدرن در دل طبیعت سبز و آرامش‌بخش، ترکیبی از معماری مدرن و محیط طبیعی باورنکردنی." },
+  { id: 4, img: "/public/Picture/beatiful buildings (1).webp", title: "آسپن هایتس", desc: "پروژه‌ای منحصر به فرد با طراحی داخلی لوکس، فضای سبز فراوان و چشم‌انداز عالی برای تجربه‌ای متفاوت از زندگی شهری." },
+];
+
+const ProjectsSection = () => {
+  return (
+    <section className={styles.projectsSection}>
+      <div className={styles.container}>
+        <h2 className={styles.heading}>
+          طیف متنوعی از پروژه‌های ما را کاوش کنید
+        </h2>
+
+        <div className={styles.grid}>
+          {projects.map((project) => (
+            <div key={project.id} className={styles.card}>
+              <img src={project.img} alt={project.title} className={styles.cardImg} />
+              <div className={styles.overlay}>
+                <p className={styles.overlayDesc}>{project.desc}</p>
+              </div>
+              <h3 className={styles.title}>{project.title}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsSection;
