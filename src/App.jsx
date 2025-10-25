013e9a03c41acb8a265c7dab7b5836e88e9f3871
Arsham-53 <sychooo847@gmail.com>
2025-10-25 12:39:48 +0330

    app

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,6 @@ import Card2 from "./components/Card with picture/Card2";
 import Comments from "./components/Comment Section/Comments";
 import Question from "./components/soal section/Question";
 import Contact from "./components/ContactSection/ContactSection";
-import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
-import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
-import Card2 from "./components/Card with picture/Card2";
-import Comments from "./components/Comment Section/Comments";
-import Question from "./components/soal section/Question";
 import Footer from "./components/footer/Footer";
 
 function App() {
